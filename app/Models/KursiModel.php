15b5e56f40effd9a1b9333966a3159c56cd81e0b
J <ocucua347506@gmail.com>
2025-11-05 13:47:24 +0900

    ubah dikit

--- app/Models/KursiModel.php
+++ app/Models/KursiModel.php
@@ -9,7 +9,14 @@ class KursiModel extends Model
     protected $stable      = 'kursi';
     protected $primaryKey  = 'id_kursi';
     protected $returntype  = 'object';
-    protected $allowedFields = ['id_studio', 'nomor_kursi', 'status_kursi'];
+    protected $allowedFields = 
+    
+    [
+        'id_room', 
+        'kode_kursi', 
+        'status'
+    ];
+    
     protected $useTimestamps = false;
     protected $useSoftDeletes = false;
    
